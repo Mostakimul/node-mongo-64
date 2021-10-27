fd4b7097463d586503908e25a9302c5c464dd8a9
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-28 01:08:05 +0200

    showing all users and deleting single user

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
 const port = 8000;
+const ObjectId = require('mongodb').ObjectId;
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -21,15 +22,27 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    // creatting the db os of exist fetch
+    // creatting the db or of exist fetch
     const database = client.db('foodMaster');
     // create a collection or fetch if alredy have
     const usersCollection = database.collection('users');
 
-    // POST API
-    app.post('/user', async (req, res) => {
+    // POST API to add user
+    app.post('/addUser', async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    // Get API for all users
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find({}).toArray();
+      // console.log(result);
+      res.send(result);
+    });
+    // Delete single user
+    app.delete('/delete-user/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await usersCollection.deleteOne({ _id: ObjectId(id) });
       res.send(result);
     });
   } finally {
